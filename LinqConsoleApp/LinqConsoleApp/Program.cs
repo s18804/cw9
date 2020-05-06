0879d738d42c6ae779dadfdf2c3ad7b8b0c2d94f
s18804 <s18804@pjwstk.edu.pl>
2020-05-06 18:15:56 +0200

    cw9

---
+++ LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            Console.WriteLine("Przyklad1");
+            d.Przyklad1();
+            Console.WriteLine("Przyklad2");
+            d.Przyklad2();
+            Console.WriteLine("Przyklad3");
+            d.Przyklad3();
+            Console.WriteLine("Przyklad4");
+            d.Przyklad4();
+            Console.WriteLine("Przyklad5");
+            d.Przyklad5();
+            Console.WriteLine("Przyklad6");
+            d.Przyklad6();
+            Console.WriteLine("Przyklad7");
+            d.Przyklad7();
+            Console.WriteLine("Przyklad8");
+            d.Przyklad8();
+            Console.WriteLine("Przyklad9");
+            d.Przyklad9();
+            Console.WriteLine("Przyklad10");
+            d.Przyklad10Button_Click();
+            Console.WriteLine("Przyklad11");
+            d.Przyklad11();
+            Console.WriteLine("Przyklad12");
+            d.Przyklad12();
+        }
+    }
+}
